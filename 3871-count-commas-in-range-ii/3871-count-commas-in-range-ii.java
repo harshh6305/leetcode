19bacf10afcb0308f756deef349d7fce45f4a2fd
harshh6305 <210000124+harshh6305@users.noreply.github.com>
2026-09-09 22:28:21 +0530

    Time: 1 ms (99.36%), Space: 43 MB (11.54%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,13 +1,22 @@
 class Solution {
     public long countCommas(long n) {
-         long ans=0;
-         long l=1000;
-        if(n<l){
-            return 0;
-        }
-        while(n>=l){
-    ans=ans+(n-l+1);
-    l=l*1000;}
-    return ans;
-        }
+        long ans = 0;
+
+        if (n >= 1000)
+            ans += n - 999;
+
+        if (n >= 1000000)
+            ans += n - 999999;
+
+        if (n >= 1000000000)
+            ans += n - 999999999;
+
+        if (n >= 1000000000000L)
+            ans += n - 999999999999L;
+
+        if (n >= 1000000000000000L)
+            ans += n - 999999999999999L;
+
+        return ans;
+    }
 }
